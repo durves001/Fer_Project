2d0748e458e2e40055943218cc7e2be2d20f4f5c
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-03-23 16:48:58 +0530

    this is first Spring boot Project for Fertilizer Project

---
+++ fertilizer_Backed/src/main/java/com/example/fertilizer_Backed/FertilizerBackedApplication.java
@@ -0,0 +1,13 @@
+package com.example.fertilizer_Backed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FertilizerBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FertilizerBackedApplication.class, args);
+	}
+
+}
---
+++ fertilizer_Backed/src/test/java/com/example/fertilizer_Backed/FertilizerBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fertilizer_Backed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FertilizerBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
